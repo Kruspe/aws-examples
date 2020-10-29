68f90bf2f72d74375a82591efaf1015e53401b25
Maximilian Prusch <maximilian.prusch@outlook.com>
2020-10-29 11:53:08 +0100

    Enhane beanstalk application

--- beanstalk-example/src/index.js
+++ beanstalk-example/src/index.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import http from 'http';
+import path from 'path';
 
 const app = express();
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.send('Main page is here to stay');
 });
 app.get('/random', (req, res) => {
     res.send(`You wanted random... Here it is ${Math.floor(Math.random() * Math.floor(100))}`);
+});
+app.get('/image', function(req, res) {
+    const imagePath = path.resolve('./image.jpg');
+    res.send("<img src='image.jpg'>Baluga</img>");
 });
 
 http.createServer(app).listen(8080, () => {
